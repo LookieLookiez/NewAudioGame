590c53ca37ecc64eb28bb4687499ddc8d34b9c3a
NicholasDuxbury <nicholasduxbury@hotmail.com>
2017-10-22 22:41:10 +1000

    AudioData Work, New Material work.

---
+++ Assets/AudioData.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+public class AudioData : MonoBehaviour {
+
+    public static AudioData Instance;
+    public AudioSource audioSource;
+
+    private float[] samples = new float[512];
+    private float[] freqBands = new float[8];
+    private float[] bandBuffer = new float[8];
+    private float[] bufferDecrease = new float[8];
+    float[] freqBandMax = new float[8];
+
+    public float[] audioBand = new float[8];
+    public float[] audioBandBuffer = new float[8];
+
+    public float amplitude;
+    public float amplitudeBuffer;
+    public float ampHighest;
+
+    void Start ()
+    {
+        Instance = this; 
+
+    }
+
+	void Update ()
+    {
+        if(audioSource != null)
+        {
+            GetSpectrumData();
+            FrequencyBands();
+            BandBuffer();
+            CreateAudioBands();
+            GetAmplitude();
+        }
+        
+    }
+
+
+    ////////////////////////////////////////////////////////////////
+    ////////////////////////////Functions///////////////////////////
+    ////////////////////////////////////////////////////////////////
+
+    //Get the spectrum data from all samples
+    void GetSpectrumData()
+    {
+        //ASk
+        //IAN
+        //IF
+        //THIS
+        //IS 
+        //TERRIBLE
+        audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+
+    void CreateAudioBands()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            if(freqBands[i] > freqBandMax[i])
+            {
+                freqBandMax[i] = freqBands[i];
+            }
+            audioBand[i] = (freqBands[i] / freqBandMax[i]);
+            audioBandBuffer[i] = (bandBuffer[i] / freqBandMax[i]);
+        }
+    }
+
+    //Split samples into frequency bands
+    void FrequencyBands()
+    {
+        var size = 0;
+        var average = 0f;
+
+        for (int i = 0; i < 8; i++)
+        {
+            int sampleSize = (int)Mathf.Pow(2, i) * 2;
+
+            if(i == 7)
+            {
+                sampleSize += 2;
+            }
+
+            for (int j = 0; j < sampleSize; j++)
+            {
+                average += samples[size] * (size + 1);
+                size++;
+                freqBands[i] = average * 10;
+            }
+        }
+    }
+
+    void BandBuffer()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            if(freqBands[i] > bandBuffer[i])
+            {
+                bandBuffer[i] = freqBands[i];
+                bufferDecrease[i] = 0.005f;
+            }
+            if(freqBands[i] < bandBuffer[i])
+            {
+                bandBuffer[i] -= bufferDecrease[i];
+                bufferDecrease[i] *= 1.2f;
+            }
+        }
+    }
+
+    void GetAmplitude()
+    {
+        float curAmp = 0;
+        float curAmpBuffer = 0;
+
+        for (int i = 0; i < 8; i++)
+        {
+            curAmp += audioBand[i];
+            curAmpBuffer += audioBandBuffer[i];
+        }
+        if(curAmp > ampHighest)
+        {
+            ampHighest = curAmp;
+        }
+        amplitude = curAmp / ampHighest;
+        amplitudeBuffer = curAmpBuffer / ampHighest;
+
+    }
+}
---
+++ Assets/GlowPulse.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowPulse : MonoBehaviour {
+    public Material blueMat;
+    public Material PinkMat;
+    public Material LeavesMat;
+
+    public int band;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(AudioData.Instance.amplitudeBuffer > 0.5f)
+        {
+            var EmisColour = new Color(blueMat.color.r, blueMat.color.g, blueMat.color.b) * AudioData.Instance.amplitudeBuffer;
+            blueMat.SetColor("_EmissionColor", EmisColour);
+        }
+
+        if (AudioData.Instance.amplitudeBuffer > 0.5f)
+        {
+            var EmisColour = new Color(LeavesMat.color.r, LeavesMat.color.g, LeavesMat.color.b) * AudioData.Instance.amplitudeBuffer;
+            LeavesMat.SetColor("_EmissionColor", EmisColour);
+        }
+        if (AudioData.Instance.audioBandBuffer[band] > 0.5f)
+        {
+            var EmisColour = new Color(PinkMat.color.r, PinkMat.color.g, PinkMat.color.b) * AudioData.Instance.audioBandBuffer[band];
+            PinkMat.SetColor("_EmissionColor", EmisColour);
+        }
+
+    }
+}
